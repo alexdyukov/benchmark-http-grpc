61b4d449d83bec41f67633761e0ef4cee5452eaa
Alex Dyukov <alex.dyukov.93@gmail.com>
2026-01-03 15:19:22 +0300

    FIX: update ci version

--- .github/update_deps.sh
+++ .github/update_deps.sh
@@ -4,7 +4,7 @@
 go test -bench=. -benchmem -benchtime=100000x > old.txt
 
 # 2. update dependencies
-go get -u ./... && go mod tidy
+go get -u ./... && go mod tidy && go get -u ./...
 
 # 3. configure git
 git config --global user.name 'update_deps robot'
@@ -13,7 +13,7 @@ git remote set-url origin https://x-access-token:${GH_TOKEN}@github.com/${REPO_N
 git checkout -b ${BRANCH_NAME}
 
 # 4. commit changes or fast exit
-git commit -am "Fix: $(date +%F) update dependencies" || exit 0
+git commit -am "FIX: $(date +%F) update dependencies" || exit 0
 git push --set-upstream origin ${BRANCH_NAME} -f || exit 0
 
 # 5. run tests after update
